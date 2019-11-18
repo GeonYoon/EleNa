05d8d0e6e5085fc14768bf8f186f79f3eba4899d
Geon Yoon <ssy01013@gmail.com>
2019-11-18 15:35:11 -0500

    remove api/ from the endpoint since our nginx will chop it up

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,5 +18,5 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/path/', include(('algorithm.urls','api-path'))),
+    path('path/', include(('algorithm.urls','api-path'))),
 ]
